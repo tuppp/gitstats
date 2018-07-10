0cbf1c32e02b661d8c6213bdba2b7f7181684302
luszuba <39732525+luszuba@users.noreply.github.com>
2018-07-10 16:00:23 +0200

    Update pyGithub.py

--- pyGithub.py
+++ pyGithub.py
@@ -17,8 +17,10 @@ i = 0
 count = 0
 perc = 0
 for r in org.get_repos():
+        
         if r.name == "leute":
         continue
+        
     print('\n\nAnalyzing Commits for '+r.name+' Repository\n')
     commits = r.get_commits()
     for x in commits:
